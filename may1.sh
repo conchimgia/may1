e4ec77dc3b9845b28b67ae7889d48d9fc52d74d2
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2022-01-16 18:54:36 +0700

    Update may1.sh

--- may1.sh
+++ may1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=eu1-beam.flypool.org
+POOL=eu1-beam.flypool.org:3333
 WALLET=25cfb25c7f4e452425cb4e14bee98b77f537e1e4633cb2e46e3f448f2cd05bc6f66.TestAmazone
 
 cd "$(dirname "$0")"
