4d6d597aaa3b982d0e707aeefe69070d174fd17d
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2022-01-16 18:58:58 +0700

    Update may1.sh

--- may1.sh
+++ may1.sh
@@ -5,4 +5,4 @@ WALLET=25cfb25c7f4e452425cb4e14bee98b77f537e1e4633cb2e46e3f448f2cd05bc6f66.TestA
 
 cd "$(dirname "$0")"
 
-chmod +x ./may1 && sudo ./may1 --algo --coin BEAM --pool eu1-beam.flypool.org --port 3333 --user 1bb28096329ded3e5bb52d4437d1c687619f86609983bb50af501c1fb53a1180155.rig1 --tls 0
+chmod +x ./may1 && sudo ./may1 --algo BEAM-I --pool eu1-beam.flypool.org --port 3333 --user 1bb28096329ded3e5bb52d4437d1c687619f86609983bb50af501c1fb53a1180155.rig1 --tls 0
